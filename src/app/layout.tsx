c57ece1b6b3f7072f545dec223902f2bb0afc760
Nyagami <hoangquan05112002@gmail.com>
2023-09-18 17:11:25 +0700

    update profile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Providers } from './providers'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Head } from 'next/document'
 
 export const metadata: Metadata = {
   title: 'IOT website',
